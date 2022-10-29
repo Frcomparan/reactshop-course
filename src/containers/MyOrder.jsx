0adf78c1a808b04a0f6bb2452063415bd7419dbf
Frcomparan <frcomparan@gmail.com>
2022-10-29 12:56:00 -0500

    Add function to remoce item from the cart

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -20,8 +20,8 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				{ state.cart.map(product => {
-					return <OrderItem product={product} key={`orderItem-${product.id}`} />
+				{ state.cart.map((product, index) => {
+					return <OrderItem indexValue={index} product={product} key={`orderItem-${index}`} />
 				}) }
 				<div className="order">
 					<p>
