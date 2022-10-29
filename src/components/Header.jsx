cb45023879806bec4948f16a7e812093e9eb1712
Frcomparan <frcomparan@gmail.com>
2022-10-29 10:20:12 -0500

    Add function to menu component and implement API

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/Header.scss';
+import Menu from '@components/Menu';
 import menuIcon from '@icons/icon_menu.svg'
 import logoMain from '@logos/logo_yard_sale.svg'
 import cartIcon from '@icons/icon_shopping_cart.svg'
 
 const Header = () => {
+	const [toggle, setToggle] = useState(false);
+
+	const handleToggle = () => {
+		setToggle(!toggle);
+
+
+	}
+
 	return (
 		<nav>
 			<img src={menuIcon} alt="menu" className="menu" />
@@ -33,13 +42,16 @@ const Header = () => {
 			</div>
 			<div className="navbar-right">
 				<ul>
-					<li className="navbar-email">platzi@example.com</li>
+					<li className="navbar-email" onClick={handleToggle}>
+						platzi@example.com
+					</li>
 					<li className="navbar-shopping-cart">
 						<img src={cartIcon} alt="shopping cart" />
 						<div>2</div>
 					</li>
 				</ul>
 			</div>
+			{ toggle && <Menu /> }
 		</nav>
 	);
 }
